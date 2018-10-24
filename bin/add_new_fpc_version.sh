7c33d9970355f035607651c8f25cf920fb1de700
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-10-24 15:16:51 +0200

    Add android-arm since FPC 3.0.4

--- bin/add_new_fpc_version.sh
+++ bin/add_new_fpc_version.sh
@@ -14,6 +14,13 @@ shift 2
 /usr/local/fpclazarus/bin/add_new_fpc_version_native.sh ${FPC_VERSION}
 /usr/local/fpclazarus/bin/add_new_fpc_version_cross.sh ${FPC_VERSION} win32 i386
 /usr/local/fpclazarus/bin/add_new_fpc_version_cross.sh ${FPC_VERSION} win64 x86_64
+
+if [ "${FPC_VERSION}" = '3.0.0' || "${FPC_VERSION}" = '3.0.2' ]; then
+  echo 'Not building cross-compiler for FPC ${FPC_VERSION}, too old'
+else
+  /usr/local/fpclazarus/bin/add_new_fpc_version_cross.sh ${FPC_VERSION} android arm
+fi
+
 /usr/local/fpclazarus/bin/add_new_fpc_version_lazarus.sh ${FPC_VERSION} ${LAZARUS_VERSION}
 
 echo "OK: FPC ${FPC_VERSION} and Lazarus ${LAZARUS_VERSION} installed completely."
