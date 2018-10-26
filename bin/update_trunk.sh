78352c0003015b1c5e90eae04d02874a90947b1c
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-10-26 05:00:13 +0200

    Do not build Android aarch64 for now, fails for unknown reason (maybe needs to start from 3.3.1?)

--- bin/update_trunk.sh
+++ bin/update_trunk.sh
@@ -44,9 +44,10 @@ make clean crossall crossinstall OS_TARGET=win64 CPU_TARGET=x86_64 INSTALL_PREFI
 make clean crossall crossinstall \
   OS_TARGET=android CPU_TARGET=arm CROSSOPT="-CfVFPV3" \
   INSTALL_PREFIX="${FPC_INSTALL_DIR}"
-make clean crossall crossinstall \
-  OS_TARGET=android CPU_TARGET=aarch64 CROSSOPT="-CfVFPV3" \
-  INSTALL_PREFIX="${FPC_INSTALL_DIR}"
+# TODO:
+# make clean crossall crossinstall \
+#   OS_TARGET=android CPU_TARGET=aarch64 CROSSOPT="-CfVFPV3" \
+#   INSTALL_PREFIX="${FPC_INSTALL_DIR}"
 
 # Set symlinks ---------------------------------------------------------------
 
