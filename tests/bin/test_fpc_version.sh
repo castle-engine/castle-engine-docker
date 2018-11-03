40b0ed4f7ab83090e4b662c8e304a0f7528d8be9
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-11-03 11:07:01 +0100

    Fix tests

--- tests/bin/test_fpc_version.sh
+++ tests/bin/test_fpc_version.sh
@@ -12,7 +12,7 @@ shift 2
 apt-get update
 apt-get --no-install-recommends -y install file
 
-/usr/local/fpclazarus/bin/test_fpc_version_native.sh ${FPC_VERSION}
-/usr/local/fpclazarus/bin/test_fpc_version_cross.sh ${FPC_VERSION} win32 i386
-/usr/local/fpclazarus/bin/test_fpc_version_cross.sh ${FPC_VERSION} win64 x86_64
-/usr/local/fpclazarus/bin/test_fpc_version_lazarus.sh ${FPC_VERSION} ${LAZARUS_VERSION}
+/usr/local/tests/bin/test_fpc_version_native.sh ${FPC_VERSION}
+/usr/local/tests/bin/test_fpc_version_cross.sh ${FPC_VERSION} win32 i386
+/usr/local/tests/bin/test_fpc_version_cross.sh ${FPC_VERSION} win64 x86_64
+/usr/local/tests/bin/test_fpc_version_lazarus.sh ${FPC_VERSION} ${LAZARUS_VERSION}
