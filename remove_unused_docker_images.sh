bfa5e6859243d967ee3c8f2afa4ba5e9345ebe82
Michalis Kamburelis <michalis.kambi@gmail.com>
2019-02-02 00:20:30 +0100

    Continue even if docker rmi failed

--- remove_unused_docker_images.sh
+++ remove_unused_docker_images.sh
@@ -6,6 +6,12 @@ echo 'Disk space before:'
 df -h /
 echo 'Removing dangling Docker images, to free disk space:'
 docker images --filter "dangling=true"
+
+# Continue even if the removal below failed with error,
+# which is possible since some job may started while the removal was in-progress
+set +e
 docker images -q --filter "dangling=true" | xargs --no-run-if-empty docker rmi
+set -e
+
 echo 'Disk space after:'
 df -h /
