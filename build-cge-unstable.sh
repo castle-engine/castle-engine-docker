09d6e7da4bf2a07156907dc09cd48dfb8a68a80b
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-06-06 13:38:00 +0200

    Upload Docker image from GHA, remove Jenkinsfile - migrating fully to GHA

--- build-cge-unstable.sh
+++ build-cge-unstable.sh
@@ -7,5 +7,4 @@ source build-common.sh
 # main ---------------------------------------------------------------------------
 
 do_build_cge unstable master
-# TODO: For now do not upload, as both Jenkins and GHA would try
-# do_upload unstable
+do_upload unstable
